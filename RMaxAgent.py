1dd94917aca35a195f9c5cba1b4ca13ebc0560d2
Eldar Shlomi <72104254+eldar702@users.noreply.github.com>
2022-05-25 14:29:32 -0700

    Update RMaxAgent.py

--- RMaxAgent.py
+++ RMaxAgent.py
@@ -24,48 +24,65 @@ class RMaxAgent(Executor):
     ##########################             Init Functions               #################################
     def __init__(self):
         super(RMaxAgent, self).__init__()
-        self.epsilon = 1
+
+
         #self.actions_list, self.states_list, self.actions_idx, self.states_idx = [], [], {}, {}
         self.action_options = ["south", "north", "west", "east"]
         self.actions_options_dict, self.actions_count_dict = {}, {}
 
     def initialize(self, services):
         self.services = services
-        self.initialize_Q_table()
+        self.init_RMax_dict()
 
-    def init_actions_counting(self):
-        for action in self.services.valid_actions.provider.parser.actions:
-            if action != "pick-food":
-                self.actions_happened_dict[action] = 0
 
-        for action in self.actions_happened_dict.keys():
-            if self.is_probabilistic(action):
-                pre_conditions = self.services.valid_actions.provider.parser.actions[action].precondition
-                for pre_cond in pre_conditions:
-                    if pre_cond.name in self.action_options:
-                        self.action_to_result_dict[pre_cond.name] = 0
-                self.action_to_result_dict[action] = pre_conditions_dict
-
-    # check with the pddl parser if its probabilistic action or deterministic
-    def is_probabilistic(self, action):
-        return hasattr(self.services.valid_actions.provider.parser.actions[action], "prob_list")
-
+    ##########################              Run Agent Run!                  #################################
     def next_action(self):
         global LAST_ACTION, LAST_STATE
         chosen_action = None
-        self.update_Q_table()
-        self.write_Q_table()
+        self.update_RMax_dict()
+        self.update_RMax_file()
 
 
-        ##############################             PDDL  Methods               #################################
-    def get_agent_location(self):
-        state = self.services.perception.get_state()
-        agent_place = list(state["at"])
-        agent_place = agent_place[0][1]
-        return agent_place
 
     #######################             R-Max  TABLE  Methods               ################################
+    def init_RMax_dict(self):
+        for action in self.services.valid_actions.provider.parser.actions:
+            if action != "pick-food":
+                self.actions_count_dict[action] = 0
+        for action in self.actions_count_dict.keys():
+            if self.is_probabilistic(action):
+                temp_dict = {}
+                conditions = self.services.valid_actions.provider.parser.actions[action].precondition
+                for condition in conditions:
+                    if condition.name in self.action_options:
+                        temp_dict[condition.name] = 0
+                if len(self.services.valid_actions.provider.parser.actions[action].prob_list) != len(temp_dict.keys()):
+                    temp_dict["stay-in-place"] = 0
+                self.actions_options_dict[action] = temp_dict
+        pass
 
+    def update_RMax_dict(self):
+
+        global LAST_ACTION, LAST_STATE
+        if LAST_STATE is None or LAST_ACTION is None:
+            return
+        # reward = self.get_reward(LAST_ACTION)
+        # state_idx, action_idx = self.states_idx[LAST_STATE], self.actions_idx[LAST_ACTION]
+
+        #update the table
+
+
+
+    ##############     @@@@@@@@@@@@@@@@@         start from here in the
+    def update_RMax_file(self):
+        new_dict = {}
+        for action in self.actions_count_dict.keys():
+            actions_probability = 0
+            new_dict[action] = actions_probability
+        f = open(policy_file_path, 'w')
+        f.write(str(new_dict))
+        f.close()
+        pass
     ####################             R-MAX - LEARNING  Methods               #############################
 
     ##############################             PDDL  Methods               #################################
@@ -74,6 +91,10 @@ class RMaxAgent(Executor):
         agent_place = list(state["at"])
         agent_place = agent_place[0][1]
         return agent_place
+
+    # check with the pddl parser if its probabilistic action or deterministic
+    def is_probabilistic(self, action):
+        return hasattr(self.services.valid_actions.provider.parser.actions[action], "prob_list")
 
 ##############################             HELPER's  Methods               #################################
 def save_dict_to_file(dic):
